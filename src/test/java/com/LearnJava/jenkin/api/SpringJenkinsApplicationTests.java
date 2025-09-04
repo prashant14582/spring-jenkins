1350aa1e9718d992984bd7440e08e1141850b4fb
prashant14582 <prashant14582@gmail.com>
2025-09-04 18:35:11 +0530

    error msg check commit

--- src/test/java/com/LearnJava/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/LearnJava/jenkin/api/SpringJenkinsApplicationTests.java
@@ -13,7 +13,7 @@ class SpringJenkinsApplicationTests {
 
 
 	@Test
-	void contextLoads() {
+	public  contextLoads() {
 		logger.info("Test case executing.....");
 		logger.info("Test case executing second log statement....");
 		assertEquals(true, true);
